80ee891ba865faa818f6eb3f4ca9e9c575d08c03
junghwan <jhzzang97@gmail.com>
2024-12-23 00:25:37 +0900

    feat: 상품 엔티티 및 조회 응답에 판매자 정보 추가
    - 상품 엔티티(ProductEntity)에 userId 필드 추가
    - 상품 조회 응답(ProductResponse)에 seller 필드 추가
    - 상품 조회 시 판매자를 확인할 수 있도록 로직 수정
    - EncryptionUtil을 사용해 판매자 이름 복호화 처리

--- src/main/java/com/example/vitaqueue/product/dto/response/ProductResponse.java
+++ src/main/java/com/example/vitaqueue/product/dto/response/ProductResponse.java
@@ -1,6 +1,7 @@
 package com.example.vitaqueue.product.dto.response;
 
 import com.example.vitaqueue.product.model.entity.ProductEntity;
+import com.example.vitaqueue.security.EncryptionUtil;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,13 +13,15 @@ public class ProductResponse {
 
     private Long id;
     private String name;
+    private String seller;
     private BigDecimal price;
     private String descript;
 
-    public static ProductResponse fromEntity(ProductEntity product) {
+    public static ProductResponse fromEntity(ProductEntity product) throws Exception {
         return new ProductResponse(
                 product.getId(),
                 product.getName(),
+                EncryptionUtil.decrypt(product.getUserId().getName()),
                 product.getPrice(),
                 product.getDescript()
         );
--- src/main/java/com/example/vitaqueue/product/service/ProductService.java
+++ src/main/java/com/example/vitaqueue/product/service/ProductService.java
@@ -8,11 +8,14 @@ import com.example.vitaqueue.product.model.entity.ProductEntity;
 import com.example.vitaqueue.product.model.entity.ProductStockEntity;
 import com.example.vitaqueue.product.repository.ProductRepository;
 import com.example.vitaqueue.product.repository.ProductStockRepository;
+import com.example.vitaqueue.user.model.entity.UserEntity;
+import com.example.vitaqueue.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
+import lombok.SneakyThrows;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,8 +23,10 @@ import java.util.List;
 public class ProductService{
 
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
     private final ProductStockRepository productStockRepository;
 
+    @SneakyThrows
     public List<ProductResponse> getProducts(Long cursorId, Integer size) {
         List<ProductEntity> productEntityList;
         // 커서가 0이라면 최신 등록 제품를 size만큼 반환
@@ -29,25 +34,38 @@ public class ProductService{
         // 커서가 0이 아니라면 cursor번 등록 제품부터 size만큼 반환
         else productEntityList = productRepository.findAllOrderByIdDescWithCursor(cursorId, size);
 
-        return productEntityList.stream().map(ProductResponse::fromEntity).toList();
+        List<ProductResponse> productResponses = new ArrayList<>();
+        for (ProductEntity productEntity : productEntityList) {
+            productResponses.add(ProductResponse.fromEntity(productEntity));
+        }
+        return productResponses;
     }
 
-    public ProductResponse getProduct(Long productId) {
+    public ProductResponse getProduct(Long productId) throws Exception {
         // 상품 아이디로 상품 조회 / 존재하지 않으면 예외처리
         ProductEntity productEntity = productRepository.findById(productId).orElseThrow(
                 () -> new VitaQueueException(ErrorCode.PRODUCT_NOT_FOUND, String.format("%d번 상품은 존재하지 않습니다.", productId)));
         return ProductResponse.fromEntity(productEntity);
     }
 
-    public void setProduct(ProductRequest request, Collection<? extends GrantedAuthority> authorities) {
+    public void setProduct(ProductRequest request, Authentication authentication) {
         // 유저 권한 확인
-        if (!authorities.equals("ROLE_ADMIN")) {
+        boolean hasAdminRole = authentication.getAuthorities().stream()
+                .anyMatch(authority -> authority.getAuthority().equals("ROLE_ADMIN"));
+        if (!hasAdminRole) {
             throw new VitaQueueException(ErrorCode.INVALID_PERMISSION, "상품 등록 권한이 없습니다.");
         }
         // 관리자 권한이면 상품 저장
-        ProductEntity productEntity = productRepository.save(new ProductEntity().of(request));
+        UserEntity userEntity = getUserEntity(authentication.getName());
+        ProductEntity productEntity = productRepository.save(new ProductEntity().of(request, userEntity));
         // 재고 정보 저장
         productStockRepository.save(new ProductStockEntity().of(productEntity, request.getStock()));
+    }
+
+    // 유저 확인
+    private UserEntity getUserEntity(String email) {
+        return userRepository.findByEmail(email).orElseThrow(
+                () -> new VitaQueueException(ErrorCode.USER_NOT_FOUND, "등록되지 않은 메일입니다."));
     }
 
 }
