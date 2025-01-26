c98c8d59b99b901ff3a31464e0724e7c74e1182f
junghwan <jhzzang97@gmail.com>
2025-01-26 15:00:20 +0900

    refactor : wishlist 반환값을 ApiResponse로 감싸서 반환.

--- wishlist-service/src/main/java/com/example/wishlistservice/client/ProductServiceClient.java
+++ wishlist-service/src/main/java/com/example/wishlistservice/client/ProductServiceClient.java
@@ -1,6 +1,7 @@
 package com.example.wishlistservice.client;
 
 
+import com.example.wishlistservice.dto.response.ApiResponse;
 import com.example.wishlistservice.dto.response.ProductResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "product-service")
 public interface ProductServiceClient {
 
-    @GetMapping("/{productId}")
-    ProductResponse getProduct(@PathVariable("productId") Long productId);
+    @GetMapping("/products/{productId}")
+    ApiResponse<ProductResponse> getProduct(@PathVariable("productId") Long productId);
 
 }
